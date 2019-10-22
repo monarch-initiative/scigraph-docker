ca20fce33234545a3e6972e673f15a54f1bf07c5
Kent Shefchek <kshefchek@gmail.com>
2019-10-22 10:36:32 -0700

    add curie map as cmd line arg

--- conf/build-service-conf.sh
+++ conf/build-service-conf.sh
@@ -8,7 +8,13 @@ else
     GRAPH_VERSION=$1
 fi
 
-wget https://raw.githubusercontent.com/monarch-initiative/dipper/master/dipper/curie_map.yaml -O $BASEDIR/curie_map.yaml
+if [ -z "$2" ]; then
+    CURIE_MAP='https://archive.monarchinitiative.org/201910/translationtable/curie_map.yaml'
+else
+    CURIE_MAP=$2
+fi
+
+wget $CURIE_MAP -O $BASEDIR/curie_map.yaml
 wget https://raw.githubusercontent.com/monarch-initiative/monarch-cypher-queries/master/src/main/cypher/dynamic-queries/cypher.yaml -O $BASEDIR/cypher.yaml
 
 sed 's/^/    /' $BASEDIR/curie_map.yaml > $BASEDIR/curies_indented.yaml
